01e7f10f3d90d516be3d2e1dc058ae437ebc0f9a
caio henrique <caiosv0720@gmail.com>
2025-06-12 16:28:47 -0300

    Update settings.py

--- teste1/settings.py
+++ teste1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-movie.onrender.com']
 
 
 
